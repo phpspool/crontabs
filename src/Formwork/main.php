ac6a00b24049f8b9650460fdc7c1d304060a653d
phpspool <yydick2001@163.com>
2021-12-02 15:51:34 +0800

    start

---
+++ src/Formwork/main.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Spool main
+ * 
+ * Class main 入口
+ * 
+ * PHP version 7.3
+ * 
+ * @category Spool
+ * @package  Formwork
+ * @author   yydick Chen <yydick@sohu.com>
+ * @license  https://spdx.org/licenses/Apache-2.0.html Apache-2.0
+ * @link     http://url.com
+ * @DateTime 2021-12-02
+ */
+
+namespace Spool\Formwork;
+
+/**
+ * Spool main
+ * 
+ * Class main 入口
+ * 
+ * PHP version 7.2
+ * 
+ * @category Spool
+ * @package  Formwork
+ * @author   yydick Chen <yydick@sohu.com>
+ * @license  https://spdx.org/licenses/Apache-2.0.html Apache-2.0
+ * @link     http://url.com
+ * @DateTime 2021-12-02
+ */
+class main
+{
+}
